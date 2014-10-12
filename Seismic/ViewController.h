e98ac7ef445454eed5af6ff563012503a1a13468
Shawn Webster <shawn@shawnwebster.com>
2014-10-12 23:51:45 +1100

    Initial Commit

---
+++ Seismic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seismic
+//
+//  Created by Shawn Webster on 2014-10-12.
+//  Copyright (c) 2014 Shawn Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
