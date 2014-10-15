96367f17fcda4c8d9893c4f8f645459ace3f9afa
Shawn Webster <shawn@shawnwebster.com>
2014-10-15 23:14:06 +1100

    cell customization for list

--- Seismic/Classes/Cells/SeismicListCell.m
+++ Seismic/Classes/Cells/SeismicListCell.m
@@ -8,6 +8,17 @@
 
 #import "SeismicListCell.h"
 #import "Earthquake.h"
+#import "NSDate+TimeAgo.h"
+
+@interface SeismicListCell()
+
+@property (strong, nonatomic) IBOutlet UILabel *lblMagnitude;
+@property (strong, nonatomic) IBOutlet UILabel *lblRegion;
+@property (strong, nonatomic) IBOutlet UILabel *lblTime;
+
++ (NSDateFormatter*) sharedDateFormatter;
+
+@end
 
 @implementation SeismicListCell
 
@@ -23,10 +34,41 @@
     // Configure the view for the selected state
 }
 
+- (NSDateFormatter*) sharedDateFormatter
+{
+    
+    static NSDateFormatter *_sharedFormatter = nil;
+    static dispatch_once_t oncePredicate;
+    dispatch_once(&oncePredicate, ^{
+        _sharedFormatter = [[NSDateFormatter alloc] init];
+        _sharedFormatter.dateFormat = @"yyyy-MM-dd' 'HH:mm:ss";
+    });
+    
+    return _sharedFormatter;
+}
+
+
 - (void) setEvent:(Earthquake *)event {
     _event = event;
     
-    self.textLabel.text = [NSString stringWithFormat:@"%@ - %@", event.magnitude, event.region];
+    //enforce .0 to display
+    self.lblMagnitude.text = [NSString stringWithFormat:@"%.1f", event.magnitude.floatValue];
+    //do something fun so the ones with higher magnitude appear more important
+    self.lblMagnitude.font = [UIFont boldSystemFontOfSize:(4.5 * event.magnitude.floatValue)];
+    
+    //an arbitrary calculation to make the magnitude text more red as the number gets larger,
+    //4 = 0.267
+    //8.4+ = 1.0
+    self.lblMagnitude.textColor = [UIColor colorWithRed:(event.magnitude.floatValue / 6.0f) - 0.4
+                                                  green:0.1f
+                                                   blue:0.1f
+                                                  alpha:1];
+    
+    self.lblRegion.text = event.region;
+    
+    NSDate *date = [self.sharedDateFormatter dateFromString:event.timedate];
+    self.lblTime.text = [date timeAgo];
+    
     
 }
 
