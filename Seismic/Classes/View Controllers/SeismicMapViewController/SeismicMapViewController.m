9c358106724c34f0b6b64a6a3d178c78a458d952
Shawn Webster <shawn@shawnwebster.com>
2014-10-15 23:15:29 +1100

    view controller for showing one/all events on map

--- Seismic/Classes/View Controllers/SeismicMapViewController/SeismicMapViewController.m
+++ Seismic/Classes/View Controllers/SeismicMapViewController/SeismicMapViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "SeismicMapViewController.h"
+#import "Earthquake.h"
+#import <MapKit/MapKit.h>
+#import "SeismicMapAnnotation.h"
 
-@interface SeismicMapViewController ()
+#import "SeismicDB.h"
+
+#define kSeismicMapAnnotationViewIdentifier @"kSeismicMapAnnotationViewIdentifier"
+
+@interface SeismicMapViewController () <MKMapViewDelegate>
+
+@property (retain, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -17,6 +26,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //showing just one event or all events?
+    NSArray *annotations = nil;
+    if (self.event) {
+        [self updateTitleForEvent:self.event];
+        annotations = [self annotationsForEvents:@[self.event]];
+    } else {
+        
+        self.title = @"All events";
+        annotations = [self annotationsForEvents:[[SeismicDB shared] events]];
+        
+    }
+    
+    [self.mapView addAnnotations:annotations];
+    
+    if (self.event) {
+        [self.mapView selectAnnotation:[annotations firstObject] animated:NO];
+    }
+}
+
+/**
+ Converts an array of Earthquake objects to SeismicMapAnnotations
+ @param events an array of Earthquake events
+ @returns An array of SeismicMapAnnotation objects
+ */
+- (NSArray*) annotationsForEvents:(NSArray*)events {
+    NSMutableArray *annotations = [NSMutableArray array];
+    
+    for (Earthquake *event in events) {
+    
+        [self.mapView setCenterCoordinate:CLLocationCoordinate2DMake(event.lat.doubleValue,
+                                                                     event.lon.doubleValue)];
+        
+        SeismicMapAnnotation *annotation = [[SeismicMapAnnotation alloc] init];
+        annotation.event = event;
+        
+        [annotations addObject:annotation];
+    
+    }
+    
+    return annotations;
+}
+
+- (void) updateTitleForEvent:(Earthquake*)event {
+    if (event) {
+        self.title = [NSString stringWithFormat:@"Magnitude %.1f", event.magnitude.floatValue];
+    } else {
+        self.title = @"All events";
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +82,54 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)dealloc {
+    [_mapView release];
+    
+    [_event release];
+    _event = nil;
+    
+    [super dealloc];
 }
-*/
+
+#pragma mark - Map View Delegate Methods
+
+- (void) mapView:(MKMapView *)mapView didDeselectAnnotationView:(MKAnnotationView *)view {
+    
+    if (!self.event) {
+        [self updateTitleForEvent:nil];
+    }
+    
+}
+
+- (void) mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
+    if (!self.event) {
+        SeismicMapAnnotation *annotation = view.annotation;
+        
+        if ([annotation isKindOfClass:[SeismicMapAnnotation class]]) {
+            [self updateTitleForEvent:annotation.event];
+        }
+    }
+}
+
+
+- (MKAnnotationView*) mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = nil;
+    
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        return nil;
+    } else {
+        annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:kSeismicMapAnnotationViewIdentifier];
+        
+        if (!annotationView) {
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+                                                          reuseIdentifier:kSeismicMapAnnotationViewIdentifier];
+            [annotationView setPinColor:MKPinAnnotationColorRed];
+            annotationView.canShowCallout = YES;
+        }
+    }
+    
+    return annotationView;
+}
+
 
 @end
